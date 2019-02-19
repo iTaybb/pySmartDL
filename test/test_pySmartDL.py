83f7417ee8508d5854c06315dd6164efcd47eeed
Itay Brandes <Brandes.Itay@gmail.com>
2019-02-19 10:54:44 +0200

    add testing for speed limiting

--- test/test_pySmartDL.py
+++ test/test_pySmartDL.py
@@ -6,6 +6,7 @@ import random
 import time
 import string
 import json
+import math
 import unittest
 
 sys.path.append('..')
@@ -21,7 +22,7 @@ class TestSmartDL(unittest.TestCase):
         while os.path.exists(self.dl_dir):
             self.dl_dir = os.path.join(os.getenv('tmp'), "".join([random.choice(string.ascii_letters+string.digits) for i in range(8)]), '')
             
-        self.default_7za920_mirrors = [
+        self.res_7za920_mirrors = [
             "https://github.com/iTaybb/pySmartDL/raw/master/test/7za920.zip",
             "https://sourceforge.mirrorservice.org/s/se/sevenzip/7-Zip/9.20/7za920.zip",
             "http://www.bevc.net/dl/7za920.zip",
@@ -29,12 +30,14 @@ class TestSmartDL(unittest.TestCase):
             "http://www.mirrorservice.org/sites/downloads.sourceforge.net/s/se/sevenzip/7-Zip/9.20/7za920.zip",
             "http://helpdesk.cki.tl/7za920.zip"
         ]
+        self.res_7za920_hash = '2a3afe19c180f8373fa02ff00254d5394fec0349f5804e0ad2f6067854ff28ac'
+        self.res_testfile_1gb = 'https://speed.hetzner.de/1GB.bin'
 
-    def set_dependencies(self):
+    def test_dependencies(self):
         self.assertTrue(sys.version_info >= (3, 4))
     
     def test_download(self):
-        obj = pySmartDL.SmartDL(self.default_7za920_mirrors, dest=self.dl_dir, progress_bar=False)
+        obj = pySmartDL.SmartDL(self.res_7za920_mirrors, dest=self.dl_dir, progress_bar=False)
         obj.start()
 
         data = obj.get_data(binary=True, bytes=2)
@@ -49,14 +52,14 @@ class TestSmartDL(unittest.TestCase):
         self.assertTrue(obj.isSuccessful())
         
     def test_hash(self):
-        obj = pySmartDL.SmartDL(self.default_7za920_mirrors, progress_bar=False)
-        obj.add_hash_verification('sha256' ,'2a3afe19c180f8373fa02ff00254d5394fec0349f5804e0ad2f6067854ff28ac') # good hash
+        obj = pySmartDL.SmartDL(self.res_7za920_mirrors, progress_bar=False)
+        obj.add_hash_verification('sha256' , self.res_7za920_hash) # good hash
         obj.start(blocking=False)  # no exceptions
         obj.wait()
         
         self.assertTrue(obj.isSuccessful())
         
-        obj = pySmartDL.SmartDL(self.default_7za920_mirrors, progress_bar=False)
+        obj = pySmartDL.SmartDL(self.res_7za920_mirrors, progress_bar=False)
         obj.add_hash_verification('sha256' ,'a'*64)  # bad hash
         obj.start(blocking=False)  # no exceptions
         obj.wait()
@@ -65,7 +68,7 @@ class TestSmartDL(unittest.TestCase):
         self.assertTrue(isinstance(obj.get_errors()[-1], pySmartDL.HashFailedException))
         
     def test_pause_unpause(self):
-        obj = pySmartDL.SmartDL(self.default_7za920_mirrors, dest=self.dl_dir, progress_bar=False)
+        obj = pySmartDL.SmartDL(self.res_7za920_mirrors, dest=self.dl_dir, progress_bar=False)
         obj.start(blocking=False)
         
         while not obj.get_dl_size():
@@ -89,7 +92,7 @@ class TestSmartDL(unittest.TestCase):
         self.assertTrue(obj.isSuccessful())
 
     def test_stop(self):
-        obj = pySmartDL.SmartDL(self.default_7za920_mirrors, dest=self.dl_dir, progress_bar=False)
+        obj = pySmartDL.SmartDL(self.res_7za920_mirrors, dest=self.dl_dir, progress_bar=False)
         obj.start(blocking=False)
 
         while not obj.get_dl_size():
@@ -98,9 +101,28 @@ class TestSmartDL(unittest.TestCase):
         obj.stop()
         obj.wait()
         self.assertFalse(obj.isSuccessful())
-        
+
+    def test_speed_limiting(self):
+        obj = pySmartDL.SmartDL(self.res_testfile_1gb, dest=self.dl_dir, progress_bar=False)
+        obj.limit_speed(1 * 1024**2)  # 1MB per sec
+        obj.start(blocking=False)
+
+        while not obj.get_dl_size():
+            time.sleep(0.1)
+
+        time.sleep(10)
+
+        expected_dl_size = 10 * 1024**2  # after 10 seconds, we should of downloaded max of 5MB
+        allowed_delta = 0.25     #  because we took only 10sec, the delta needs to be quite big, it we were to test 30sec the delta would probably be much smaller
+        diff = math.fabs(expected_dl_size - obj.get_dl_size()) / expected_dl_size
+
+        obj.stop()
+        obj.wait()
+
+        self.assertLessEqual(diff, allowed_delta)
+    
     def test_basic_auth(self):
-        basic_auth_test_url = "http://httpbin.org/basic-auth/user/passwd"
+        basic_auth_test_url = "https://httpbin.org/basic-auth/user/passwd"
         obj = pySmartDL.SmartDL(basic_auth_test_url, progress_bar=False)
         obj.add_basic_authentication('user', 'passwd')
         obj.start()
@@ -108,7 +130,7 @@ class TestSmartDL(unittest.TestCase):
         self.assertTrue(json.loads(data)['authenticated'])
         
     def test_unicode(self):
-        url = "http://he.wikipedia.org/wiki/ג'חנון"
+        url = "https://he.wikipedia.org/wiki/ג'חנון"
         obj = pySmartDL.SmartDL(url, progress_bar=False)
         obj.start()
         
