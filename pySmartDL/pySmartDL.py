4b2f042fb3cc8d6897e61c35b1a6cbd1eeebfda5
Dash <26944764+DashLt@users.noreply.github.com>
2019-07-21 10:38:03 -0400

    revert url param change

--- pySmartDL/pySmartDL.py
+++ pySmartDL/pySmartDL.py
@@ -49,7 +49,7 @@ class SmartDL:
     The main SmartDL class
     
     :param urls: Download url. It is possible to pass unsafe and unicode characters. You can also pass a list of urls, and those will be used as mirrors.
-    :type urls: string or list of string
+    :type urls: string or list of strings
     :param requestArgs: Arguments to be passed to a new urllib.request.Request instance in dictionary form. See https://docs.python.org/3/library/urllib.request.html#urllib.request.Request for options. 
     :type requestArgs: dict
     :param dest: Destination path. Default is `%TEMP%/pySmartDL/`.
